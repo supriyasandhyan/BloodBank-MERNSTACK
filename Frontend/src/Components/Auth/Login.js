4c6f08a27a02e88df0ef85873a7efdad3ed342d9
Supriya Premraj Sandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2024-08-10 17:34:16 +0530

    login and registration functionaily

--- Frontend/src/Components/Auth/Login.js
+++ Frontend/src/Components/Auth/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Form from "../Reused/Forms";
+import banner from '../../Images/loginbanner.jpg'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+        <div className="row g-0">
+          <div className="col-md-8 form-banner">
+            <img src={banner} alt="loginImage" />
+          </div>
+          <div className="col-md-4 form-container">
+            <Form
+              formTitle={"Login Page"}
+              submitBtn={"Login"}
+              formType={"login"}
+            />
+          </div>
+        </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
